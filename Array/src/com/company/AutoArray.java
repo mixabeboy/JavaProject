b3fe3be4ea3a7557cd77233c7b10fd903e5a2852
unknown <mixabeboy.net@yandex,ru>
2019-07-17 21:45:29 +0300

    Mihan

---
+++ Array/src/com/company/AutoArray.java
@@ -0,0 +1,6 @@
+class AutoArray {
+    public static void main(String[] args) {
+        int month_day[] ={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("В июне" + month_day[6] + "Дней");
+    }
+}
